f2afbc0c3b34c7c2fb6064542a471d094868c0ec
Dovydas Petrutis <Dovydas.Petrutis@atea.lt>
2018-08-02 17:18:00 +0300

    Adding Genres create/edit

---
+++ ClientApp/components/GenreForm.tsx
@@ -0,0 +1,139 @@
+import * as React from "react";
+import { RouteComponentProps } from "react-router";
+import { Link, NavLink } from "react-router-dom";
+import { Genre } from "./Genres";
+interface GenreFormState {
+  title: string;
+  loading: boolean;
+  genre: Genre;
+}
+export class GenreForm extends React.Component<
+  RouteComponentProps<{}>,
+  GenreFormState
+> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      loading: true,
+      genre: new Genre()
+    };
+
+    var id = this.props.match.params["id"];
+    // This will set state for Edit genre
+    if (id > 0) {
+      fetch("api/genres/" + id)
+        .then(response => response.json() as Promise<Genre>)
+        .then(data => {
+          this.setState({ title: "Edit", loading: false, genre: data });
+        });
+    }
+    // This will set state for Add genre
+    else {
+      this.state = {
+        title: "Create",
+        loading: false,
+
+        genre: new Genre()
+      };
+    }
+    // This binding is necessary to make "this" work in the callback
+    this.handleSave = this.handleSave.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+  }
+  public render() {
+    let contents = this.state.loading ? (
+      <p>
+        <em>Loading...</em>
+      </p>
+    ) : (
+      this.renderCreateForm()
+    );
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <h3>Genre</h3>
+        <hr />
+        {contents}
+      </div>
+    );
+  }
+  // This will handle the submit form event.
+  private handleSave(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    // PUT request for Edit employee.
+    if (this.state.genre.id) {
+      fetch("/api/genres", {
+        method: "PUT",
+        body: data
+      })
+        .then(response => response.json())
+        .then(responseJson => {
+          this.props.history.push("/genres");
+        });
+    }
+    // POST request for Add employee.
+    else {
+      fetch("/api/genres", {
+        method: "POST",
+        body: data
+      })
+        .then(response => response.json())
+        .then(responseJson => {
+          this.props.history.push("/genres");
+        });
+    }
+  }
+  // This will handle Cancel button click event.
+  private handleCancel(e) {
+    e.preventDefault();
+    this.props.history.push("/genres");
+  }
+  // Returns the HTML Form to the render() method.
+  private renderCreateForm() {
+    return (
+      <form onSubmit={this.handleSave}>
+        <div className="form-group row">
+          <input type="hidden" name="id" value={this.state.genre.id} />
+        </div>
+        <div className="form-group row">
+          <label className=" control-label col-md-12" htmlFor="name">
+            Name
+          </label>
+          <div className="col-md-4">
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              defaultValue={this.state.genre.name}
+              required
+            />
+          </div>
+        </div>
+        <div className="form-group row">
+          <label className="control-label col-md-12" htmlFor="Description">
+            Description
+          </label>
+          <div className="col-md-4">
+            <textarea
+              className="form-control"
+              name="Description"
+              style={{ height: 150, width: 500 }}
+              defaultValue={this.state.genre.description}
+              required
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <button type="submit" className="btn btn-default">
+            Save
+          </button>
+          <button className="btn" onClick={this.handleCancel}>
+            Cancel
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
--- ClientApp/routes.tsx
+++ ClientApp/routes.tsx
@@ -1,14 +1,20 @@
-import * as React from 'react';
-import { Route } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
-import { Genres } from './components/Genres';
+import * as React from "react";
+import { Route } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { Home } from "./components/Home";
+import { FetchData } from "./components/FetchData";
+import { Counter } from "./components/Counter";
+import { Genres } from "./components/Genres";
+import { GenreForm } from "./components/GenreForm";
 
-export const routes = <Layout>
-    <Route exact path='/' component={ Home } />
-    <Route path='/counter' component={ Counter } />
-    <Route path='/fetchdata' component={ FetchData } />
-    <Route path='/genres' component={ Genres } />
-</Layout>;
+export const routes = (
+  <Layout>
+    <Route exact path="/" component={Home} />
+    <Route path="/counter" component={Counter} />
+    <Route path="/fetchdata" component={FetchData} />
+
+    <Route path="/genres" component={Genres} />
+    <Route path="/genre/new" component={GenreForm} />
+    <Route path="/genre/edit/:id" component={GenreForm} />
+  </Layout>
+);
